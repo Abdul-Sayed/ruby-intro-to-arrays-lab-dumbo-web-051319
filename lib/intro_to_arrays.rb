564722cfb7a7f8ec5bd5463dbc7064fa14f5109f
Abdul <awsayed@alaska.edu>
2019-04-29 03:58:52 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,34 @@
+
+
+def instantiate_new_array
+	return my_new_array = []
+end
+
+def array_with_two_elements
+	return my_two_array = [1,2]
+end
+
+
+def first_element(array)
+	return first_element(0)
+end
+
+def third_element(array)
+	return array(2)
+end 
+
+def last_element(array)
+	return last_element(array)
+end 
+
+def first_element_with_array_methods(array)
+	return array.first
+end 
+
+def last_element_with_array_methods(array)
+	return array.last
+end 
+
+def length_of_array(arr)
+	return arr.length
+end 
